ad7ca883bf7f2eff056651710f8394d710f4edd5
Socrattin <socrattin@gmail.com>
2017-07-25 15:27:58 +0800

    ting

--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "I am the best!" << std::endl;
+    std::cout << "I am the best!" << std::endl;
+    std::cout << "I am the best!" << std::endl;
+    
+
     return 0;
 }
