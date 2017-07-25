a9b46b7e73df4e67ee533fab0209395a7b0dda40
Socrattin <socrattin@gmail.com>
2017-07-25 15:33:12 +0800

    l,mxk

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main() {
     std::cout << "I am the best!" << std::endl;
     std::cout << "I am the best!" << std::endl;
     std::cout << "I am the best!" << std::endl;
-    
+    std::cout << "I am the best!" << std::endl;
+
 
     return 0;
 }
